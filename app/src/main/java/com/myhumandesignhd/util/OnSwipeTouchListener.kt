38ff04e5f775af6cbef7fe1a6bc5939edd5aaac1
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-01-23 17:16:22 +0600

    update to android 14

--- app/src/main/java/com/myhumandesignhd/util/OnSwipeTouchListener.kt
+++ app/src/main/java/com/myhumandesignhd/util/OnSwipeTouchListener.kt
@@ -21,15 +21,15 @@ open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
         }
 
         override fun onFling(
-            e1: MotionEvent,
+            e1: MotionEvent?,
             e2: MotionEvent,
             velocityX: Float,
             velocityY: Float
         ): Boolean {
             var result = false
             try {
-                val diffY = e2.y - e1.y
-                val diffX = e2.x - e1.x
+                val diffY = e2.y - (e1?.y ?: 0f)
+                val diffX = e2.x - (e1?.x ?: 0f)
                 if (abs(diffX) > abs(diffY)) {
                     if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                         if (diffX > 0) {
